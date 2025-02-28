f58e8b0487f5e174a5da2f1f6ec57e1278bc2c65
Saman Yousefisohi <samix.ys@gmail.com>
2025-03-01 00:31:41 +0100

    add command-line flags for fullscreen and tracing options in main.go

--- cmd/desktop/main.go
+++ cmd/desktop/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 	"os"
 
 	"github.com/samix73/game/internal/game"
 )
 
+var (
+	fullscreen = flag.Bool("fullscreen", false, "enable fullscreen mode")
+	tracing    = flag.Bool("tracing", false, "enable tracing")
+)
+
 func main() {
+	flag.Parse()
+
 	g := game.NewGame(&game.Config{
 		Title:        "Game",
 		ScreenWidth:  1280,
 		ScreenHeight: 960,
-		Fullscreen:   true,
-		Tracing:      true,
+		Fullscreen:   *fullscreen,
+		Tracing:      *tracing,
 	})
 
 	if err := g.Start(); err != nil {
