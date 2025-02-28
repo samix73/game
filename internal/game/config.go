136adf793c27fe9ec0edd82749075e40f4531b59
Saman Yousefisohi <samix.ys@gmail.com>
2025-03-01 00:22:20 +0100

    refactor game configuration; move Config struct to game.go and enhance tracing setup

--- internal/game/config.go
+++
@@ -1,9 +0,0 @@
-package game
-
-type Config struct {
-	Title                     string
-	ScreenWidth, ScreenHeight int
-	Fullscreen                bool
-	Tracing                   bool
-	TraceFile                 string
-}
--- internal/game/game.go
+++ internal/game/game.go
@@ -11,6 +11,14 @@ import (
 
 var _ ebiten.Game = (*Game)(nil)
 
+type Config struct {
+	Title                     string
+	ScreenWidth, ScreenHeight int
+	Fullscreen                bool
+	Tracing                   bool
+	TraceFile                 string
+}
+
 type Game struct {
 	cfg *Config
 
@@ -28,20 +36,13 @@ func (g *Game) Start() error {
 	ebiten.SetFullscreen(g.cfg.Fullscreen)
 	ebiten.SetWindowTitle(g.cfg.Title)
 
-	if g.cfg.Tracing {
-		filename := fmt.Sprintf("trace_%s.out",
-			time.Now().UTC().Format("2006-01-02_15-04-05"),
-		)
-		f, err := os.Create(filename)
-		if err != nil {
-			return fmt.Errorf("game.Game.setupTrace os.Create error: %w", err)
-		}
-		defer f.Close()
+	closer, err := g.setupTrace()
+	if err != nil {
+		return fmt.Errorf("game.Game.Start g.setupTrace error: %w", err)
+	}
 
-		if err := trace.Start(f); err != nil {
-			return fmt.Errorf("game.Game.setupTrace trace.Start error: %w", err)
-		}
-		defer trace.Stop()
+	if closer != nil {
+		defer closer()
 	}
 
 	if err := ebiten.RunGameWithOptions(g, nil); err != nil {
@@ -49,6 +50,29 @@ func (g *Game) Start() error {
 	}
 
 	return nil
+}
+
+func (g *Game) setupTrace() (func(), error) {
+	if g.cfg.Tracing {
+		filename := fmt.Sprintf("trace_%s.out",
+			time.Now().UTC().Format("2006-01-02_15-04-05"),
+		)
+		f, err := os.Create(filename)
+		if err != nil {
+			return nil, fmt.Errorf("game.Game.setupTrace os.Create error: %w", err)
+		}
+
+		if err := trace.Start(f); err != nil {
+			return nil, fmt.Errorf("game.Game.setupTrace trace.Start error: %w", err)
+		}
+
+		return func() {
+			f.Close()
+			trace.Stop()
+		}, nil
+	}
+
+	return nil, nil
 }
 
 func (g *Game) SetActiveLevel(level *Level) {
