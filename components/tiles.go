409f75355eef0b5449d37fa6d9590cfb71620c12
Saman Yousefisohi <samix.ys@gmail.com>
2025-09-12 11:20:32 +0200

    feat: implement TileSystem for managing TileMap rendering and validation

--- components/tiles.go
+++ components/tiles.go
@@ -14,6 +14,10 @@ type TileMap struct {
 	Width, Height int
 	Layer         int
 	Tiles         []int // Width * Height; each int is an index into the tileset; -1 = empty
+	Atlas         *ebiten.Image
+	TileSize      int
+	Columns       int
+	Sub           []*ebiten.Image
 }
 
 func (t *TileMap) Reset() {
@@ -21,9 +25,17 @@ func (t *TileMap) Reset() {
 	t.Height = 0
 	t.Layer = 0
 	t.Tiles = make([]int, 0)
+	t.Atlas.Deallocate()
+	t.TileSize = 0
+	t.Columns = 0
+	t.Sub = nil
 }
 
 func (t *TileMap) Init() {
+	if t.Atlas == nil || t.TileSize <= 0 {
+		return
+	}
+
 	if t.Tiles != nil || t.Width <= 0 || t.Height <= 0 {
 		return
 	}
@@ -32,6 +44,26 @@ func (t *TileMap) Init() {
 	for i := range t.Tiles {
 		t.Tiles[i] = -1
 	}
+
+	w := t.Atlas.Bounds().Dx()
+	h := t.Atlas.Bounds().Dy()
+
+	t.Columns = w / t.TileSize
+	rows := h / t.TileSize
+	count := t.Columns * rows
+
+	t.Sub = make([]*ebiten.Image, 0, count)
+	for id := range count {
+		x := (id % t.Columns) * t.TileSize
+		y := (id / t.Columns) * t.TileSize
+
+		subImage := t.Atlas.SubImage(
+			image.Rect(x, y, x+t.TileSize, y+t.TileSize),
+		).(*ebiten.Image)
+		t.Sub = append(t.Sub, subImage)
+	}
+
+	t.Columns = t.Atlas.Bounds().Dx() / t.TileSize
 }
 
 func (t *TileMap) index(x, y int) int {
@@ -62,50 +94,10 @@ func (t *TileMap) Set(x, y, id int) error {
 	return nil
 }
 
-var _ ecs.Component = (*TileSet)(nil)
-
-type TileSet struct {
-	Atlas    *ebiten.Image
-	TileSize int
-	Columns  int
-	Sub      []*ebiten.Image
-}
-
-func (t *TileSet) Reset() {
-	t.Atlas.Deallocate()
-	t.TileSize = 0
-	t.Columns = 0
-	t.Sub = nil
-}
-
-func (t *TileSet) At(id int) *ebiten.Image {
+func (t *TileMap) ImageAt(id int) *ebiten.Image {
 	if id < 0 || id >= len(t.Sub) {
 		return nil
 	}
 
 	return t.Sub[id]
-}
-
-func (t *TileSet) Init() {
-	if t.Atlas == nil || t.TileSize <= 0 {
-		return
-	}
-
-	w := t.Atlas.Bounds().Dx()
-	h := t.Atlas.Bounds().Dy()
-
-	t.Columns = w / t.TileSize
-	rows := h / t.TileSize
-	count := t.Columns * rows
-
-	t.Sub = make([]*ebiten.Image, 0, count)
-	for id := range count {
-		x := (id % t.Columns) * t.TileSize
-		y := (id / t.Columns) * t.TileSize
-
-		subImage := t.Atlas.SubImage(
-			image.Rect(x, y, x+t.TileSize, y+t.TileSize),
-		).(*ebiten.Image)
-		t.Sub = append(t.Sub, subImage)
-	}
 }
---
+++ systems/tiles.go
@@ -0,0 +1,90 @@
+package systems
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	ecs "github.com/samix73/ebiten-ecs"
+	"github.com/samix73/game/components"
+)
+
+var _ ecs.System = (*TileSystem)(nil)
+
+type TileSystem struct {
+	*ecs.BaseSystem
+}
+
+func NewTileSystem(priority int) *TileSystem {
+	return &TileSystem{
+		BaseSystem: ecs.NewBaseSystem(ecs.NextID(), priority),
+	}
+}
+
+func (t *TileSystem) validateTileMap(tm *components.TileMap) bool {
+	if tm.Width <= 0 || tm.Height <= 0 {
+		return false
+	}
+
+	if tm.Atlas == nil || tm.TileSize <= 0 || tm.Columns <= 0 {
+		return false
+	}
+
+	if len(tm.Tiles) != tm.Width*tm.Height {
+		return false
+	}
+
+	return true
+}
+
+func (t *TileSystem) buildTileSetImage(tm *components.TileMap) *ebiten.Image {
+	img := ebiten.NewImage(tm.Width*tm.TileSize, tm.Height*tm.TileSize)
+
+	for y := 0; y < tm.Height; y++ {
+		for x := 0; x < tm.Width; x++ {
+			tileIndex := tm.At(x, y)
+			if tileIndex < 0 {
+				continue
+			}
+
+			tileImg := tm.ImageAt(tileIndex)
+			if tileImg == nil {
+				continue
+			}
+
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(x*tm.TileSize), float64(y*tm.TileSize))
+			img.DrawImage(tileImg, op)
+		}
+	}
+
+	return img
+}
+
+func (t *TileSystem) buildTileSet(entity ecs.EntityID, tm *components.TileMap) {
+	if !t.validateTileMap(tm) {
+		return
+	}
+
+	em := t.EntityManager()
+
+	renderable, ok := ecs.GetComponent[components.Renderable](em, entity)
+	if !ok {
+		renderable = ecs.AddComponent[components.Renderable](em, entity)
+	}
+
+	renderable.Sprite = t.buildTileSetImage(tm)
+	renderable.Order = tm.Layer
+	renderable.GeoM = ebiten.GeoM{}
+}
+
+func (t *TileSystem) Update() error {
+	em := t.EntityManager()
+
+	for entity := range ecs.Query[components.TileMap](em) {
+		tm := ecs.MustGetComponent[components.TileMap](em, entity)
+
+		t.buildTileSet(entity, tm)
+	}
+
+	return nil
+}
+
+func (t *TileSystem) Teardown() {}
